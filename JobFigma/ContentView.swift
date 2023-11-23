0bec0ef4f40f018d50386b93421316aeed0b21e8
Demian Füglistaler <demianfueglistaler@icloud..com>
2023-11-23 10:14:12 +0100

    Initial Commit

---
+++ JobFigma/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JobFigma
+//
+//  Created by Demian Füglistaler on 23.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JobFigma/JobFigmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobFigmaApp.swift
+//  JobFigma
+//
+//  Created by Demian Füglistaler on 23.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct JobFigmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
